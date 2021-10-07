bb9303c3f5ebb8d2ee3fa9080d30c334f629bf25
brijesh353 <brijesh@zorem.com>
2021-10-07 12:40:58 +0530

    settings updated

--- includes/views/map-providers.php
+++ includes/views/map-providers.php
@@ -1,56 +1,52 @@
-<div class="d_table" style="">		
+<div class="d_table">		
 	<form method="post" id="trackship_mapping_form" action="" enctype="multipart/form-data">
-		<div class="outer_form_table">				
-			<table class="form-table heading-table">
-				<tbody>
-					<tr valign="top">
-						<td>
-							<h1><?php esc_html_e( 'Map Shipping Providers', 'trackship-for-woocommerce' ); ?></h1>
-						</td>
-					</tr>
-				</tbody>
-			</table>
-			<table class="form-table fixed map-provider-table">
-				<thead>
-                	<p><?php esc_html_e( 'If you get different names from your shipping service, you can map the Shipping Providers names to the ones on TrackShip.', 'trackship-for-woocommerce' ); ?></p>
-					<tr class="ptw_provider_border">
-						<th><?php esc_html_e( 'Shipping Provider', 'trackship-for-woocommerce' ); ?></th>
-						<th><?php esc_html_e( 'TrackShip Provider', 'trackship-for-woocommerce' ); ?></th>
-					</tr>
-				</thead>
-			<tbody>
-			<?php 
-			$trackship_map_provider = get_option( 'trackship_map_provider' );
-			$ts_shippment_providers = $this->get_trackship_provider();
-			if ( !empty( $trackship_map_provider ) ) :
-				foreach ( $trackship_map_provider as $key => $val ) : 
-					?>
-					<tr>
-						<td>
-							<input type="text" class="map_shipping_provider_text" name="detected_provider[]" value="<?php esc_html_e( $key ); ?>">
-						</td>
-						<td>
-							<select name="ts_provider[]" class="select2">
-								<option value=""><?php esc_html_e( 'Select' ); ?></option>
-								<?php foreach ( $ts_shippment_providers as $ts_provider ) { ?>
-									<option value="<?php echo esc_html( $ts_provider->ts_slug ); ?>" <?php esc_html_e( $ts_provider->ts_slug == $val ? 'selected' : '' ); ?> ><?php echo esc_html( $ts_provider->provider_name ); ?></option>	
-								<?php } ?>
-							</select>
-							<span class="dashicons dashicons-trash remove_custom_maping_row"></span>
-						</td>
-					</tr>
-				<?php endforeach; ?>
-			<?php endif; ?>
-		</tbody>
-	</table>	
-	<button class="button-primary add_custom_mapping_h3 button-trackship"><?php esc_html_e('Add mapping', 'trackship-for-woocommerce' ); ?><span class="dashicons dashicons-plus ptw-dashicons"></span></button><div class="add-custom-mapping spinner" style="float:none;"></div>
+		<div class="heading_panel section_mapping_heading">
+			<strong><?php esc_html_e( 'Map Shipping Providers', 'trackship-for-woocommerce' ); ?></strong>
+			<div class="heading_panel_save">
+				<span class="dashicons dashicons-arrow-right-alt2"></span>
+				<div class="spinner"></div>
+				<button name="save" class="button-primary btn_green2 btn_large woocommerce-save-button button-trackship" type="submit"><?php esc_html_e( 'Save & close', 'trackship-for-woocommerce' ); ?></button>
+				<?php wp_nonce_field( 'trackship_mapping_form', 'trackship_mapping_form_nonce' ); ?>
+				<input type="hidden" name="action" value="trackship_mapping_form_update">
+			</div>
 		</div>
-        <div class="settings_ul_submit">								
-            <button name="save" class="button-primary btn_green2 btn_large woocommerce-save-button button-trackship" type="submit"><?php esc_html_e( 'Save Changes', 'trackship-for-woocommerce' ); ?></button>
-            <div class="mapping-save spinner"></div>						
-            <?php wp_nonce_field( 'trackship_mapping_form', 'trackship_mapping_form_nonce' ); ?>
-            <input type="hidden" name="action" value="trackship_mapping_form_update">
-        </div>	
-	</form>	
-    
-</div>				
+		<div class="panel_content section_mapping_content">
+			<div class="outer_form_table">
+				<table class="form-table fixed map-provider-table">
+					<thead>
+						<p><?php esc_html_e( 'If you get different names from your shipping service, you can map the Shipping Providers names to the ones on TrackShip.', 'trackship-for-woocommerce' ); ?></p>
+						<tr class="ptw_provider_border">
+							<th><?php esc_html_e( 'Shipping Provider', 'trackship-for-woocommerce' ); ?></th>
+							<th><?php esc_html_e( 'TrackShip Provider', 'trackship-for-woocommerce' ); ?></th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						$trackship_map_provider = get_option( 'trackship_map_provider' );
+						$ts_shippment_providers = $this->get_trackship_provider();
+						if ( !empty( $trackship_map_provider ) ) :
+							foreach ( $trackship_map_provider as $key => $val ) : 
+								?>
+								<tr>
+									<td>
+										<input type="text" class="map_shipping_provider_text" name="detected_provider[]" value="<?php esc_html_e( $key ); ?>">
+									</td>
+									<td>
+										<select name="ts_provider[]" class="select2">
+											<option value=""><?php esc_html_e( 'Select' ); ?></option>
+											<?php foreach ( $ts_shippment_providers as $ts_provider ) { ?>
+												<option value="<?php echo esc_html( $ts_provider->ts_slug ); ?>" <?php esc_html_e( $ts_provider->ts_slug == $val ? 'selected' : '' ); ?> ><?php echo esc_html( $ts_provider->provider_name ); ?></option>	
+											<?php } ?>
+											</select>
+										<span class="dashicons dashicons-trash remove_custom_maping_row"></span>
+									</td>
+								</tr>
+							<?php endforeach; ?>
+						<?php endif; ?>
+					</tbody>
+				</table>	
+				<button class="button-primary add_custom_mapping_h3 button-trackship"><?php esc_html_e('Add mapping', 'trackship-for-woocommerce' ); ?><span class="dashicons dashicons-plus ptw-dashicons"></span></button><div class="add-custom-mapping spinner"></div>
+			</div>
+		</div>
+	</form>
+</div>
--- includes/views/settings.php
+++ includes/views/settings.php
@@ -3,70 +3,94 @@ $completed_order_with_tracking = $this->completed_order_with_tracking();
 $completed_order_with_zero_balance = $this->completed_order_with_zero_balance();							
 $completed_order_with_do_connection = $this->completed_order_with_do_connection();
 $total_orders = $completed_order_with_tracking + $completed_order_with_zero_balance + $completed_order_with_do_connection;
-$cookie = isset( $_COOKIE["Notice"] ) ? $_COOKIE["Notice"] : '';
+$cookie = isset( $_COOKIE['Notice'] ) ? sanitize_text_field( $_COOKIE['Notice'] ) : '';
 if ( 'delete' != $cookie && $total_orders > 0 ) { ?>
-    <div class="tools_tab_ts4wc">
-        <div class="trackship-notice" style="border: 0;">
-            <?php //%s used for replacement ?>
-            <p><?php echo sprintf( esc_html( 'We detected %s Shipped orders from the last 30 days that were not sent to TrackShip, you can bulk send them to TrackShip', 'trackship-for-woocommerce'), esc_html( $total_orders ) ) ; ?><span style="float:right;padding-top: 7px;font-weight: 600;" class="dashicons remove-icon dashicons-no-alt"></span></p>
-            <button style="float: none;" class="button-primary button-trackship bulk_shipment_status_button" <?php echo 0 == $total_orders ? 'disabled' : ''; ?>><?php esc_html_e( 'Get Shipment Status', 'trackship-for-woocommerce' ); ?></button>
-        </div>
-    </div>
-<?php } ?>
-<form method="post" id="wc_ast_trackship_form" action="" enctype="multipart/form-data">
-	<div class="outer_form_table">
-		<table class="form-table heading-table">
-			<tbody>				
-				<tr valign="top">
-					<td>
-						<h1 style=""><?php esc_html_e( 'Settings', 'trackship-for-woocommerce' ); ?></h1>
-					</td>
-				</tr>
-			</tbody>
-		</table>		
-		<?php $this->get_html_ul( $this->get_trackship_general_data() ); ?>												
+	<div class="tools_tab_ts4wc">
+		<div class="trackship-notice" style="border: 0;">
+			<?php //%s used for replacement ?>
+			<p><?php printf( esc_html__( 'We detected %s Shipped orders from the last 30 days that were not sent to TrackShip, you can bulk send them to TrackShip', 'trackship-for-woocommerce' ), esc_html( $total_orders ) ); ?><span class="dashicons remove-icon dashicons-no-alt"></span></p>
+			<button class="button-primary button-trackship bulk_shipment_status_button" <?php echo 0 == $total_orders ? 'disabled' : ''; ?>><?php esc_html_e( 'Get Shipment Status', 'trackship-for-woocommerce' ); ?></button>
+		</div>
 	</div>
-    <div class="outer_form_table">
-    	<div style="margin:15px 0;">
-            <input type="hidden" name="wc_ast_status_delivered" value="0"/>
-            <input class="ast-tgl ast-tgl-flat ts_order_status_toggle" id="wc_ast_status_delivered" name="wc_ast_status_delivered" type="checkbox" <?php echo get_option( 'wc_ast_status_delivered' ) ? 'checked' : ''; ?> value="1"/>
-            <label class="ast-tgl-btn ast-tgl-btn-green" for="wc_ast_status_delivered"></label>
-            <span style="margin-left: 5px;"><?php esc_html_e( 'Enable Order Delivery Automation', 'trackship-for-woocommerce' ); ?></span>
-            <span class="woocommerce-help-tip tipTip" title="<?php esc_html_e( 'Enable a Custom Order Status Delivered that will be set automatically when all the order shipments are delivered', 'trackship-for-woocommerce' ); ?>"></span>
+<?php } ?>
+<div class="accordion_container">
+	<form method="post" id="wc_ast_trackship_form" action="" enctype="multipart/form-data">
+		<div class="outer_form_table">
+			<div class="heading_panel section_settings_heading">
+				<strong><?php esc_html_e( 'General Settings', 'trackship-for-woocommerce' ); ?></strong>
+				<div class="heading_panel_save">
+					<span class="dashicons dashicons-arrow-right-alt2"></span>
+					<div class="spinner"></div>
+					<button name="save" class="button-primary button-trackship btn_large woocommerce-save-button" type="submit" value="Save & close">
+						<?php esc_html_e( 'Save & close', 'trackship-for-woocommerce' ); ?>
+					</button>
+					<?php wp_nonce_field( 'wc_ast_trackship_form', 'wc_ast_trackship_form_nonce' ); ?>
+					<input type="hidden" name="action" value="wc_ast_trackship_form_update">
+				</div>
+			</div>
+			<div class="panel_content section_settings_content">
+				<?php $this->get_html_ul( $this->get_trackship_general_data() ); ?>
+				<div class="settings_toogle">
+					<input type="hidden" name="wc_ast_status_delivered" value="0"/>
+					<input class="ast-tgl ast-tgl-flat ts_order_status_toggle" id="wc_ast_status_delivered" name="wc_ast_status_delivered" type="checkbox" <?php echo get_option( 'wc_ast_status_delivered' ) ? 'checked' : ''; ?> value="1"/>
+					<label class="ast-tgl-btn ast-tgl-btn-green" for="wc_ast_status_delivered"></label>
+					<label class="setting_ul_tgl_checkbox_label">
+						<span><?php esc_html_e( 'Enable Order Delivery Automation', 'trackship-for-woocommerce' ); ?></span>
+						<span class="woocommerce-help-tip tipTip" title="<?php esc_html_e( 'Enable a Custom Order Status Delivered that will be set automatically when all the order shipments are delivered', 'trackship-for-woocommerce' ); ?>"></span>
+					</label>
+				</div>
+				<div class="ts4wc_delivered_color">
+					<div class="order-label wc-delivered">
+						<?php 
+						if ( get_option('wc_ast_status_delivered') ) {
+							esc_html_e( wc_get_order_status_name( 'delivered' ), 'trackship-for-woocommerce' );	
+						} else {
+							esc_html_e( 'Delivered', 'trackship-for-woocommerce' );
+						}
+						?>
+					</div>
+					<input class="input-text regular-input color_input" type="text" name="wc_ast_status_label_color" id="wc_ast_status_label_color" value="<?php echo esc_html( get_option( 'wc_ast_status_label_color', '#09d3ac' ) ); ?>" placeholder="">
+					<select class="select ts_custom_order_color_select" id="wc_ast_status_label_font_color" name="wc_ast_status_label_font_color">	
+					<option value="#fff" <?php echo '#fff' == get_option('wc_ast_status_label_font_color', '#fff') ? 'selected' : ''; ?>> <?php esc_html_e( 'Light Font', 'trackship-for-woocommerce' ); ?>
+					</option>
+						<option value="#000" <?php echo '#000' == get_option('wc_ast_status_label_font_color', '#fff') ? 'selected' : ''; ?>><?php esc_html_e( 'Dark Font', 'trackship-for-woocommerce' ); ?>
+						</option>
+					</select>							
+				</div>
+				<?php
+				$late_shipments_email_settings = get_option('late_shipments_email_settings');
+				$wcast_late_shipments_days = isset( $late_shipments_email_settings['wcast_late_shipments_days'] ) ? $late_shipments_email_settings['wcast_late_shipments_days'] : '';
+				?>
+				<div class="late_shipment_days_settings">
+					<label><?php esc_html_e('Number of days for late shipments', 'trackship-for-woocommerce'); ?></label>	
+					<input class="input-text" type="number" name="wcast_late_shipments_days" id="wcast_late_shipments_days" min="1" value="<?php echo esc_html( $wcast_late_shipments_days ); ?>">
+				</div>
+			</div>
 		</div>
-        <div class="ts4wc_delivered_color">
-        	<div class="order-label wc-delivered">
-				<?php 
-                if ( get_option('wc_ast_status_delivered') ) {
-                    esc_html_e( wc_get_order_status_name( 'delivered' ), 'trackship-for-woocommerce' );	
-                } else {
-                    esc_html_e( 'Delivered', 'trackship-for-woocommerce' );
-                }
-                ?>
-            </div>
-            <input class="input-text regular-input color_input" type="text" name="wc_ast_status_label_color" id="wc_ast_status_label_color" style="" value="<?php echo esc_html( get_option( 'wc_ast_status_label_color', '#09d3ac' ) ); ?>" placeholder="">
-            <select class="select ts_custom_order_color_select" id="wc_ast_status_label_font_color" name="wc_ast_status_label_font_color">	
-                <option value="#fff" <?php echo '#fff' == get_option('wc_ast_status_label_font_color', '#fff') ? 'selected' : ''; ?>> <?php esc_html_e( 'Light Font', 'trackship-for-woocommerce' ); ?>
-                </option>
-                <option value="#000" <?php echo '#000' == get_option('wc_ast_status_label_font_color', '#fff') ? 'selected' : ''; ?>><?php esc_html_e( 'Dark Font', 'trackship-for-woocommerce' ); ?>
-                </option>
-            </select>							
+	</form>
+	<form method="post" id="trackship_tracking_page_form" action="" enctype="multipart/form-data">
+		<div class="heading_panel section_tracking_page_heading">
+			<strong><?php esc_html_e( 'Tracking Page', 'trackship-for-woocommerce' ); ?></strong>
+			<div class="heading_panel_save">
+				<span class="dashicons dashicons-arrow-right-alt2"></span>
+				<div class="spinner"></div>
+				<button name="save" class="button-primary button-trackship btn_large woocommerce-save-button" type="submit" value="Save & close">
+					<?php esc_html_e( 'Save & close', 'trackship-for-woocommerce' ); ?>
+				</button>
+				<?php wp_nonce_field( 'trackship_tracking_page_form', 'trackship_tracking_page_form_nonce' ); ?>
+				<input type="hidden" name="action" value="trackship_tracking_page_form_update">
+			</div>
 		</div>
-        <?php $late_shipments_email_settings = get_option('late_shipments_email_settings');
-		$wcast_late_shipments_days = isset( $late_shipments_email_settings['wcast_late_shipments_days'] ) ? $late_shipments_email_settings['wcast_late_shipments_days'] : ''; ?>
-        <div style="margin-top:10px">
-            <label style="margin-right: 20px;" for=""><?php esc_html_e('Number of days for late shipments', 'trackship-for-woocommerce'); ?></label>	
-            <input class="input-text" type="number" name="wcast_late_shipments_days" id="wcast_late_shipments_days" min="1" value="<?php echo esc_html( $wcast_late_shipments_days ); ?>" style="width: 50px;">
+		<div class="panel_content section_tracking_page_content">
+			<div class="outer_form_table">
+				<?php $this->get_html_ul( $this->get_tracking_page_data() ); ?>													
+			</div>
 		</div>
-        <div class="settings_ul_submit" style="margin-top: 15px;">								
-            <button name="save" class="button-primary button-trackship btn_large" type="submit" value="Save changes">
-                <?php esc_html_e( 'Save Changes', 'trackship-for-woocommerce' ); ?>
-            </button>
-            <div class="spinner"></div>
-			<?php wp_nonce_field( 'wc_ast_trackship_form', 'wc_ast_trackship_form_nonce' ); ?>
-            <input type="hidden" name="action" value="wc_ast_trackship_form_update">
-        </div>      	    
-    </div>
-</form>
-<?php do_action( 'after_trackship_settings' );
-	
+	</form>
+	<?php
+	do_action( 'after_trackship_settings' );
+	if ( !is_plugin_active( 'ast-pro/ast-pro.php' ) ) {
+		include __DIR__ . '/map-providers.php';
+	}
+	?>
+</div>
