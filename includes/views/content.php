dd4da9b47391526c627fc694dc8f68f0b03965d6
brijesh353 <brijesh@zorem.com>
2021-11-08 15:30:40 +0530

    Code improvement

--- includes/views/content.php
+++ includes/views/content.php
@@ -8,49 +8,43 @@ $menu_tab = isset( $_GET[ 'tab' ] ) ? sanitize_text_field( $_GET[ 'tab' ] ) : 's
 	<span class="woocommerce-layout__activity-panel"><?php include 'header-sidebar.php'; ?></span>
 	<div class="trackship_admin_content" >
 		<div class="trackship_nav_div">	
-			<?php if ( trackship_for_woocommerce()->is_trackship_connected() ) { ?>
-				<?php
-				$array = array(
-					array(
-						'label'	=> __( 'Settings', 'trackship-for-woocommerce' ),
-						'slug'	=> 'settings',
-						'show'	=> true,
-					),
-					array(
-						'label'	=> __( 'Notifications', 'trackship-for-woocommerce' ),
-						'slug'	=> 'notifications',
-						'show'	=> true,
-					),
-					array(
-						'label'	=> __( 'Tools', 'trackship-for-woocommerce' ),
-						'slug'	=> 'tools',
-						'show'	=> true,
-					),
-				);
-				?>
-				<div>
-					<?php foreach ( $array as $key => $val ) { ?>
-						<input id="tab_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" type="radio" name="tabs" class="tab_input" data-label="<?php esc_html_e( $val[ 'label' ] ); ?>" data-tab="<?php esc_html_e( $val[ 'slug' ] ); ?>" <?php echo $val[ 'slug' ] == $menu_tab ? 'checked' : ''; ?> >
-						<label for="tab_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" class="tab_label <?php echo 'settings' == $val[ 'slug' ] ? 'first_label' : ''; ?>">
-							<?php esc_html_e( $val[ 'label' ] ); ?>
-						</label>
-					<?php } ?>					
-				</div>
-				<div class="menu_devider"></div>
-				<?php foreach ( $array as $key => $val ) { ?>
-					<?php if ( $val[ 'show' ] ) { ?>
-						<section id="content_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" class="inner_tab_section">
-							<div class="tab_inner_container">
-								<?php include __DIR__ . '/' . $val[ 'slug' ] . '.php'; ?>
-							</div>
-						</section>
-					<?php } ?>
-				<?php } ?>
 			<?php
-			} else {
-				include 'trackship-integration.php';
-			}
+			$array = array(
+				array(
+					'label'	=> __( 'Settings', 'trackship-for-woocommerce' ),
+					'slug'	=> 'settings',
+					'show'	=> true,
+				),
+				array(
+					'label'	=> __( 'Notifications', 'trackship-for-woocommerce' ),
+					'slug'	=> 'notifications',
+					'show'	=> true,
+				),
+				array(
+					'label'	=> __( 'Tools', 'trackship-for-woocommerce' ),
+					'slug'	=> 'tools',
+					'show'	=> true,
+				),
+			);
 			?>
+			<div>
+				<?php foreach ( $array as $key => $val ) { ?>
+					<input id="tab_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" type="radio" name="tabs" class="tab_input" data-label="<?php esc_html_e( $val[ 'label' ] ); ?>" data-tab="<?php esc_html_e( $val[ 'slug' ] ); ?>" <?php echo $val[ 'slug' ] == $menu_tab ? 'checked' : ''; ?> >
+					<label for="tab_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" class="tab_label <?php echo 'settings' == $val[ 'slug' ] ? 'first_label' : ''; ?>">
+						<?php esc_html_e( $val[ 'label' ] ); ?>
+					</label>
+				<?php } ?>					
+			</div>
+			<div class="menu_devider"></div>
+			<?php foreach ( $array as $key => $val ) { ?>
+				<?php if ( $val[ 'show' ] ) { ?>
+					<section id="content_trackship_<?php esc_html_e( $val[ 'slug' ] ); ?>" class="inner_tab_section">
+						<div class="tab_inner_container">
+							<?php include __DIR__ . '/' . $val[ 'slug' ] . '.php'; ?>
+						</div>
+					</section>
+				<?php } ?>
+			<?php } ?>
 		</div>                   					
    </div>				
 </div>
