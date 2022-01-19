84cd55d47e33ee8fde4f1aeb258c4cc4cbac87f5
brijesh353 <brijesh@zorem.com>
2022-01-19 15:45:41 +0530

    tracking page header design updated

--- includes/views/front/tracking_page_header.php
+++ includes/views/front/tracking_page_header.php
@@ -6,6 +6,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 <div class="tracking-header">
 	<?php
 	do_action( 'trackship_tracking_header_before', $order->get_id(), $tracker, $provider_name, $tracking_number );
+	$row = trackship_for_woocommerce()->actions->get_tracking_shipment_row( $order->get_id(), $tracking_number );
 	?>
 
 	<div class="tracking_number_wrap">
@@ -29,9 +30,23 @@ if ( ! defined( 'ABSPATH' ) ) {
 						<strong><?php esc_html_e( $tracking_number ); ?></strong>	
 					<?php } ?>
 				</li>
+				<?php if ( !$hide_last_mile && $row->delivery_provider && $row->delivery_number ) { ?>
+					<li class="last_mile_tracking_number">
+						<?php $delivery_provider = trackship_for_woocommerce()->actions->get_provider_name( $row->delivery_provider ); ?>
+						<span><?php echo esc_html( $delivery_provider ); ?> <?php esc_html_e( 'last-mile tracking number', 'trackship-for-woocommerce' ); ?></span>
+						<strong> <?php echo esc_html( $row->delivery_number ) ?></strong>
+					</li>
+				<?php } ?>
 			</ul>
-		</div>
+		</div>	
 	</div>
+	<?php if ( !$hide_from_to && $row->origin_country && $row->destination_country && $row->destination_country != $row->origin_country ) { ?>
+		<div class="shipping_from_to">
+			<span class="shipping_from"><?php echo esc_html( WC()->countries->countries[ $row->origin_country ] ) ?></span>
+			<img class="shipping_to_img" src="<?php echo esc_url( trackship_for_woocommerce()->plugin_dir_url() ); ?>assets/images/arrow.png">
+			<span class="shipping_to"><?php echo esc_html( WC()->countries->countries[ $row->destination_country ] ) ?></span>
+		</div>
+	<?php } ?>
 	<div class="shipment_status_heading <?php esc_html_e( $tracker->ep_status ); ?>">
 		<?php
 		if ( in_array( $tracker->ep_status, array( 'pending_trackship', 'pending', 'carrier_unsupported', 'unknown', 'insufficient_balance' ) ) ) {
@@ -41,8 +56,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 		}
 		?>
 	</div>
-
-	<?php if ( $tracker->est_delivery_date ) { ?>
+	<?php $show_est_delivery_date = apply_filters( 'show_est_delivery_date', true, $provider_name ); ?>
+	<?php if ( $tracker->est_delivery_date && $show_est_delivery_date ) { ?>
 		<span class="est-delivery-date tracking-number">
 			<?php esc_html_e( 'Est. Delivery Date', 'trackship-for-woocommerce' ); ?> : 
 			<strong><?php esc_html_e( date_i18n( 'l, M d', strtotime( $tracker->est_delivery_date ) ) ); ?></strong>
