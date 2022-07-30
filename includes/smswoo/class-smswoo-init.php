9e970617e26b15b964ed599d5796a9f16d8a83b3
brijesh353 <brijesh@zorem.com>
2022-07-30 15:35:50 +0530

    SMSWOO design update

--- includes/smswoo/class-smswoo-init.php
+++ includes/smswoo/class-smswoo-init.php
@@ -42,23 +42,25 @@ class tswc_smswoo_init {
 		require_once 'class-smswoo-admin.php';
 		$this->smswoo_admin = tswc_smswoo_admin::get_instance();
 		
-		//sms-notification
-		require_once 'class-smswoo-sms-notification.php';
-		$this->sms_notification = tswc_smswoo_sms_notification::get_instance();
-		
-		//sms-gateway
-		require_once 'class-smswoo-sms-gateway.php';
-		
-		//include all provider
-		if ( in_array( get_option( 'user_plan' ), array( 'Free Trial', 'Free 50', 'No active plan' ) ) ) {
-			return;
+		if ( ! function_exists( 'SMSWOO' ) && !is_plugin_active( 'zorem-sms-for-woocommerce/zorem-sms-for-woocommerce.php' ) ) {
+			//sms-notification
+			require_once 'class-smswoo-sms-notification.php';
+			$this->sms_notification = tswc_smswoo_sms_notification::get_instance();
+			
+			//sms-gateway
+			require_once 'class-smswoo-sms-gateway.php';
+			
+			//include all provider
+			if ( in_array( get_option( 'user_plan' ), array( 'Free Trial', 'Free 50', 'No active plan' ) ) ) {
+				return;
+			}
+			require_once 'services/class-smswoo-nexmo.php';
+			require_once 'services/class-smswoo-twilio.php';
+			require_once 'services/class-smswoo-clicksend.php';
+			require_once 'services/class-smswoo-fast2sms.php';
+			require_once 'services/class-smswoo-msg91.php';
+			require_once 'services/class-smswoo-smsalert.php';
 		}
-		require_once 'services/class-smswoo-nexmo.php';
-		require_once 'services/class-smswoo-twilio.php';
-		require_once 'services/class-smswoo-clicksend.php';
-		require_once 'services/class-smswoo-fast2sms.php';
-		require_once 'services/class-smswoo-msg91.php';
-		require_once 'services/class-smswoo-smsalert.php';
 	}
 	
 }
