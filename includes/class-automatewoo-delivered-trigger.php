2dcd9f7fb31fa565a24ee34a2dfb0a26dfc9c8bb
brijesh353 <83744659+brijesh353@users.noreply.github.com>
2022-09-26 16:58:16 +0530

    AutomateWoo compatibility added

---
+++ includes/class-automatewoo-delivered-trigger.php
@@ -0,0 +1,21 @@
+<?php
+
+// phpcs:ignoreFile
+
+namespace AutomateWoo;
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+/**
+ * @class Trigger_Order_Completed
+ */
+class Trigger_Order_Delivered extends Trigger_Abstract_Order_Status_Base {
+
+	public $_target_status = 'delivered';
+
+	function load_admin_details() {
+		parent::load_admin_details();
+		$this->title = __( 'Order Delivered', 'trackship-for-woocommerce' );
+	}
+
+}
