5ecdb2b33ce9a26c197b234d6c691c2541b93159
brijesh353 <brijesh@trackship.com>
2024-03-20 10:29:57 +0530

    Default enabled shipment status

--- includes/class-wc-trackship-install.php
+++ includes/class-wc-trackship-install.php
@@ -49,27 +49,24 @@ class WC_Trackship_Install {
 		if ( version_compare( get_option( 'trackship_db' ), '1.0', '<' ) ) {
 			update_trackship_settings( 'wc_ast_use_tracking_page', 1 );
 
-			$data1 = $data2 = $data3 = [];
-			$data1['wcast_enable_availableforpickup_email'] = 1;
-			update_option( 'wcast_availableforpickup_email_settings', $data1 );
+			$availableforpickup_data = get_option( 'wcast_availableforpickup_email_settings', array() );
+			$availableforpickup_data['wcast_enable_availableforpickup_email'] = 1;
+			update_option( 'wcast_availableforpickup_email_settings', $availableforpickup_data );
 
-			$data2['wcast_enable_outfordelivery_email'] = 1;
-			update_option( 'wcast_outfordelivery_email_settings', $data2 );
+			$outfordelivery_data = get_option( 'wcast_outfordelivery_email_settings', array() );
+			$outfordelivery_data['wcast_enable_outfordelivery_email'] = 1;
+			update_option( 'wcast_outfordelivery_email_settings', $outfordelivery_data );
 
-			$data3['wcast_enable_delivered_status_email'] = 1;
-			update_option( 'wcast_delivered_status_email_settings', $data3 );
-
-			update_option( 'wcast_enable_availableforpickup_email', '1.0' );
-			update_option( 'trackship_db', '1.0' );
-			update_option( 'trackship_db', '1.0' );
-			
-			update_option( 'trackship_db', '1.0' );
+			$delivered_data = get_option( 'wcast_delivered_status_email_settings', array() );
+			$delivered_data['wcast_enable_delivered_status_email'] = 1;
+			update_option( 'wcast_delivered_status_email_settings', $delivered_data );
 
 			$this->create_shipping_provider_table();
 			$this->update_shipping_providers();
 			$this->create_shipment_table();
 			$this->create_shipment_meta_table();
 			$this->create_email_log_table();
+			update_option( 'trackship_db', '1.0' );
 		}
 
 		if ( version_compare( get_option( 'trackship_db' ), '1.8', '<' ) ) {
