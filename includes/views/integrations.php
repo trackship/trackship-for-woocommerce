f3f47a3be20b8e78c27ad7cea097f41696f7c838
Brijesh patel <brijesh@trackship.com>
2025-10-09 15:16:30 +0530

    Documentation link added in integration

--- includes/views/integrations.php
+++ includes/views/integrations.php
@@ -9,15 +9,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 			<?php
 			$integrations = $this->get_trackship_integrations_data();
 			foreach ( $integrations as $key => $value ) {
-				if ( $value['value'] ) {
-					$checked = 'checked';
-				} else {
-					$checked = '';
-				}
+				$checked = ! empty( $value['value'] ) ? 'checked' : '';
 				?>
 				<div class="ts-grid-item">
 					<div class="ts_integration_image"><img src="<?php echo esc_url($value['image']); ?>"></div>
-					<div class="ts_integration_title"><?php echo esc_html($value['title']); ?></div>
+					<div class="ts_integration_title">
+						<?php echo esc_html( $value['title'] ); ?>
+						<?php if ( $value['docs'] ) : ?>
+							<div class="ts_integration_doc">
+								<a href="<?php echo esc_url( $value['docs'] ); ?>" target="_blank"><?php esc_html_e( 'Documentation', 'trackship-for-woocommerce' ); ?></a>
+							</div>
+						<?php endif; ?>
+					</div>
 					<div class="ts_integration_checkbox">
 						<input type="hidden" name="<?php echo esc_html( $key ); ?>" value="0"/>
 						<input class="tgl tgl-flat" id="<?php echo esc_html( $key ); ?>" name="<?php echo esc_html( $key ); ?>" type="checkbox" <?php echo esc_html( $checked ); ?> value="1"/>
